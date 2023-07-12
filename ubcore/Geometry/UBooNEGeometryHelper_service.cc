7e228792c2465e98993b690724dd1cbe629b8722
Herbert Greenlee <greenlee@fnal.gov>
2023-07-12 18:36:36 -0500

    Use cetmodules.

--- ubcore/Geometry/UBooNEGeometryHelper_service.cc
+++ ubcore/Geometry/UBooNEGeometryHelper_service.cc
@@ -1,39 +1,4 @@
-////////////////////////////////////////////////////////////////////////////////
-/// \file UBOONEGeometryHelper_service.cc
-///
-/// \version $Id
-/// \author  rs@fnal.gov
-////////////////////////////////////////////////////////////////////////////////
-
-// Migration note:
-// Geometry --> ubcore/Geometry
 #include "ubcore/Geometry/UBooNEGeometryHelper.h"
-
-#include "larcorealg/Geometry/ChannelMapAlg.h"
-#include "larcorealg/Geometry/GeometryCore.h" // larcore. geo::GeometryData_t
-
 #include "art/Framework/Services/Registry/ServiceDefinitionMacros.h"
-
-// Migration note:
-// Geometry --> ubcore/Geometry for the two below
-#include "ubcore/Geometry/ChannelMapUBooNEAlg.h"
-
-namespace uboone {
-
-  UBooNEGeometryHelper::UBooNEGeometryHelper(fhicl::ParameterSet const& pset)
-    : fPset(pset)
-  {}
-
-  std::unique_ptr<geo::ChannelMapAlg>
-  UBooNEGeometryHelper::doConfigureChannelMapAlg(fhicl::ParameterSet const& sortingParameters,
-                                                 std::string const& detectorName) const
-  {
-    if ( detectorName.find("microboone") == std::string::npos ) {
-      std::cout << __PRETTY_FUNCTION__ << ": WARNING USING CHANNEL MAP ALG WITH NON-MICROBOONE GEO!" << std::endl;
-    }
-    return std::make_unique<geo::ChannelMapUBooNEAlg>(fPset, sortingParameters);
-  }
-
-}
 
 DEFINE_ART_SERVICE_INTERFACE_IMPL(uboone::UBooNEGeometryHelper, geo::ExptGeoHelperInterface)
